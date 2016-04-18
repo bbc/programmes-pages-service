db592e6052105eb028e60f37189ffc0da090cf6f
David Marland <david.marland@bbc.co.uk>
2016-04-18 14:15:23 +0100

    Adding new relationship fields. Old ones will be removed once the data has been moved into the new columns

--- src/Data/ProgrammesDb/Entity/RefRelationship.php
+++ src/Data/ProgrammesDb/Entity/RefRelationship.php
@@ -39,6 +39,30 @@ class RefRelationship
     private $objectPid;
 
     /**
+     * @var string|null
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $subjectId;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $subjectType;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $objectId;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", nullable=false)
+     */
+    private $objectType;
+
+    /**
      * @var RefRelationshipType
      *
      * @ORM\ManyToOne(targetEntity="RefRelationshipType")
@@ -90,6 +114,58 @@ class RefRelationship
     public function setObjectPid(string $objectPid = null)
     {
         $this->objectPid = $objectPid;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSubjectId()
+    {
+        return $this->subjectId;
+    }
+
+    public function setSubjectId(string $subjectId = null)
+    {
+        $this->subjectId = $subjectId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getSubjectType()
+    {
+        return $this->subjectType;
+    }
+
+    public function setSubjectType(string $subjectType = null)
+    {
+        $this->subjectType = $subjectType;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getObjectId()
+    {
+        return $this->objectId;
+    }
+
+    public function setObjectId(string $objectId = null)
+    {
+        $this->objectId = $objectId;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getObjectType()
+    {
+        return $this->objectType;
+    }
+
+    public function setObjectType(string $objectType = null)
+    {
+        $this->objectType = $objectType;
     }
 
     /**
