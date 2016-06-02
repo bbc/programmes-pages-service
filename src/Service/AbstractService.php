3ba41f042e6ef80f974575328e743b08cf1fb8a8
Ben Scott <ben.scott@bbc.co.uk>
2016-06-02 10:37:45 +0100

    Extract common Service functionality into an AbstractService

---
+++ src/Service/AbstractService.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace BBC\ProgrammesPagesService\Service;
+
+use BBC\ProgrammesPagesService\Mapper\MapperInterface;
+use Doctrine\ORM\EntityRepository;
+
+abstract class AbstractService
+{
+    const DEFAULT_PAGE = 1;
+    const DEFAULT_LIMIT = 50;
+
+    /**
+     * @var MapperInterface
+     */
+    protected $mapper;
+
+    /**
+     * @var EntityRepository
+     */
+    protected $repository;
+
+    public function __construct(
+        EntityRepository $repository,
+        MapperInterface $mapper
+    ) {
+        $this->repository = $repository;
+        $this->mapper = $mapper;
+    }
+
+    protected function getOffset($limit, $page): int
+    {
+        return $limit * ($page - 1);
+    }
+
+    protected function mapSingleEntity($dbEntity)
+    {
+        if (is_null($dbEntity)) {
+            return null;
+        }
+
+        return $this->mapper->getDomainModel($dbEntity);
+    }
+
+    protected function mapManyEntities(array $dbEntities): array
+    {
+        return array_map([$this, 'mapSingleEntity'], $dbEntities);
+    }
+}
--- src/Service/ProgrammesService.php
+++ src/Service/ProgrammesService.php
@@ -7,38 +7,30 @@ use BBC\ProgrammesPagesService\Domain\Entity\Programme;
 use BBC\ProgrammesPagesService\Domain\ValueObject\Pid;
 use BBC\ProgrammesPagesService\Mapper\ProgrammesDbToDomain\ProgrammeMapper;
 
-class ProgrammesService
+class ProgrammesService extends AbstractService
 {
-    const DEFAULT_PAGE = 1;
-    const DEFAULT_LIMIT = 50;
-
-    protected $coreEntityRepository;
-
-    protected $programmeMapper;
-
     public function __construct(
-        CoreEntityRepository $coreEntityRepository,
-        ProgrammeMapper $programmeMapper
+        CoreEntityRepository $repository,
+        ProgrammeMapper $mapper
     ) {
-        $this->coreEntityRepository = $coreEntityRepository;
-        $this->programmeMapper = $programmeMapper;
+        parent::__construct($repository, $mapper);
     }
 
     public function findAll(
         int $limit = self::DEFAULT_LIMIT,
         int $page = self::DEFAULT_PAGE
     ): array {
-        $dbEntities = $this->coreEntityRepository->findAllWithParents(
+        $dbEntities = $this->repository->findAllWithParents(
             $limit,
             $this->getOffset($limit, $page)
         );
 
-        return $this->mapManyProgrammeEntities($dbEntities);
+        return $this->mapManyEntities($dbEntities);
     }
 
     public function countAll(): int
     {
-        return $this->coreEntityRepository->countAll();
+        return $this->repository->countAll();
     }
 
     /**
@@ -46,9 +38,9 @@ class ProgrammesService
      */
     public function findByPidFull(Pid $pid)
     {
-        $dbEntity = $this->coreEntityRepository->findByPidFull($pid);
+        $dbEntity = $this->repository->findByPidFull($pid);
 
-        return $this->mapSingleProgrammeEntity($dbEntity);
+        return $this->mapSingleEntity($dbEntity);
     }
 
     public function findEpisodeGuideChildrenByPid(
@@ -56,30 +48,30 @@ class ProgrammesService
         int $limit = self::DEFAULT_LIMIT,
         int $page = self::DEFAULT_PAGE
     ): array {
-        $parent = $this->coreEntityRepository->findByPidFull((string) $pid);
+        $parent = $this->repository->findByPidFull((string) $pid);
 
         if (is_null($parent)) {
             return [];
         }
 
-        $dbEntities = $this->coreEntityRepository->findEpisodeGuideChildren(
+        $dbEntities = $this->repository->findEpisodeGuideChildren(
             $parent['id'],
             $limit,
             $this->getOffset($limit, $page)
         );
 
-        return $this->mapManyProgrammeEntities($dbEntities);
+        return $this->mapManyEntities($dbEntities);
     }
 
     public function countEpisodeGuideChildrenByPid(Pid $pid): int
     {
-        $parent = $this->coreEntityRepository->findByPidFull((string) $pid);
+        $parent = $this->repository->findByPidFull((string) $pid);
 
         if (is_null($parent)) {
             return 0;
         }
 
-        return $this->coreEntityRepository->countEpisodeGuideChildren($parent['id']);
+        return $this->repository->countEpisodeGuideChildren($parent['id']);
     }
 
     public function findDescendantsByPid(
@@ -90,36 +82,17 @@ class ProgrammesService
         // in order for this to be efficient, we need to know the original programme database ID.
         // @todo - investigate another way to do this so we don't need this effectively redundant query
 
-        $dbEntity = $this->coreEntityRepository->findByPidFull($pid);
+        $dbEntity = $this->repository->findByPidFull($pid);
         if (!$dbEntity) {
             return null;
         }
 
-        $dbEntities = $this->coreEntityRepository->findDescendants(
+        $dbEntities = $this->repository->findDescendants(
             $dbEntity,
             $limit,
             $this->getOffset($limit, $page)
         );
 
-        return $this->mapManyProgrammeEntities($dbEntities);
-    }
-
-    protected function getOffset($limit, $page): int
-    {
-        return $limit * ($page - 1);
-    }
-
-    protected function mapSingleProgrammeEntity($dbEntity)
-    {
-        if (is_null($dbEntity)) {
-            return null;
-        }
-
-        return $this->programmeMapper->getDomainModel($dbEntity);
-    }
-
-    protected function mapManyProgrammeEntities(array $dbEntities): array
-    {
-        return array_map([$this, 'mapSingleProgrammeEntity'], $dbEntities);
+        return $this->mapManyEntities($dbEntities);
     }
 }
