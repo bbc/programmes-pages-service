5ee0ad7f0caa04b6609e8e9f2dd1e4c562671481
Martin Buckley <martin.buckley@bbc.co.uk>
2016-07-06 20:55:30 +0100

    Adding tests

--- tests/Service/ProgrammesService/ProgrammesServiceFindSiblingByProgrammeTest.php
+++ tests/Service/ProgrammesService/ProgrammesServiceFindSiblingByProgrammeTest.php
@@ -40,7 +40,27 @@ class ProgrammesServiceFindSiblingByProgrammeTest extends AbstractProgrammesServ
         $this->assertEquals($this->programmeFromDbData($dbData), $result);
     }
 
-    public function testFindNextSiblingByProgrammePrefersSearchingByPositionIfPositionAndReleaseDateArePresent()
+    public function testFindNextSiblingByProgrammeSearchesByFirstBroadcastDate()
+    {
+        $programme = $this->getMockEpisode(1, null, null, new \DateTimeImmutable('2000-01-01 00:00:00'));
+        $dbData = ['pid' => 'b010t19z'];
+
+        $this->mockRepository->expects($this->never())
+            ->method('findAdjacentProgrammeByPosition');
+
+        $this->mockRepository->expects($this->never())
+            ->method('findAdjacentProgrammeByReleaseDate');
+
+        $this->mockRepository->expects($this->once())
+            ->method('findAdjacentProgrammeByFirstBroadcastDate')
+            ->with(1, new \DateTimeImmutable('2000-01-01 00:00:00'), 'Episode', 'next')
+            ->willReturn($dbData);
+
+        $result = $this->service()->findNextSiblingByProgramme($programme);
+        $this->assertEquals($this->programmeFromDbData($dbData), $result);
+    }
+
+    public function testFindNextSiblingByProgrammePrefersSearchingByPosition()
     {
         $programme = $this->getMockEpisode(1, 3, new PartialDate('2016'));
         $dbData = ['pid' => 'b010t19z'];
@@ -57,7 +77,7 @@ class ProgrammesServiceFindSiblingByProgrammeTest extends AbstractProgrammesServ
         $this->assertEquals($this->programmeFromDbData($dbData), $result);
     }
 
-    public function testFindNextSiblingByProgrammeFallsBackToReleaseDateIfPositionAndReleaseDateArePresent()
+    public function testFindNextSiblingByProgrammeFallsBackToReleaseDate()
     {
         $programme = $this->getMockEpisode(1, 3, new PartialDate('2016'));
         $dbData = ['pid' => 'b010t19z'];
@@ -70,6 +90,25 @@ class ProgrammesServiceFindSiblingByProgrammeTest extends AbstractProgrammesServ
         $this->mockRepository->expects($this->once())
             ->method('findAdjacentProgrammeByReleaseDate')
             ->with(1, new PartialDate('2016'), 'Episode', 'next')
+            ->willReturn($dbData);
+
+        $result = $this->service()->findNextSiblingByProgramme($programme);
+        $this->assertEquals($this->programmeFromDbData($dbData), $result);
+    }
+
+    public function testFindNextSiblingByProgrammeFallsBackToFirstBroadcastDate()
+    {
+        $programme = $this->getMockEpisode(1, 3, null, new \DateTimeImmutable('2016-01-01 00:00:00'));
+        $dbData = ['pid' => 'b010t19z'];
+
+        $this->mockRepository->expects($this->once())
+            ->method('findAdjacentProgrammeByPosition')
+            ->with(1, 3, 'Episode', 'next')
+            ->willReturn(null);
+
+        $this->mockRepository->expects($this->once())
+            ->method('findAdjacentProgrammeByFirstBroadcastDate')
+            ->with(1, new \DateTimeImmutable('2016-01-01 00:00:00'), 'Episode', 'next')
             ->willReturn($dbData);
 
         $result = $this->service()->findNextSiblingByProgramme($programme);
@@ -214,11 +253,12 @@ class ProgrammesServiceFindSiblingByProgrammeTest extends AbstractProgrammesServ
         $this->assertNull($result);
     }
 
-    private function getMockEpisode($parentId = null, $position = null, $releaseDate = null)
+    private function getMockEpisode($parentId = null, $position = null, $releaseDate = null, $firstBroadcastDate = null)
     {
         $programme = $this->mockEntity('Episode');
         $programme->method('getPosition')->willReturn($position);
         $programme->method('getReleaseDate')->willReturn($releaseDate);
+        $programme->method('getFirstBroadcastDate')->willReturn($firstBroadcastDate);
 
         if ($parentId) {
             $parent = $this->mockEntity('Series');
