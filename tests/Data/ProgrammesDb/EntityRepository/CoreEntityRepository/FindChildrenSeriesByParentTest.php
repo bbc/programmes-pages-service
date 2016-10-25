143737a3561fbef9f73735ca4a33fa2ebf735c46
Felipe Araujo <felipe.peixoto.araujo@gmail.com>
2016-10-25 16:58:47 +0100

    Added limit and offset tests

--- tests/Data/ProgrammesDb/EntityRepository/CoreEntityRepository/FindChildrenSeriesByParentTest.php
+++ tests/Data/ProgrammesDb/EntityRepository/CoreEntityRepository/FindChildrenSeriesByParentTest.php
@@ -21,7 +21,36 @@ class FindChildrenSeriesByParentTest extends AbstractDatabaseTest
         $expectedPids = ['b00swyx1', 'b010t150'];
         $this->assertEquals($expectedPids, array_column($entities, 'pid'));
 
-        // findAll query and the parent lookup query
+        $this->assertCount(2, $this->getDbQueries());
+    }
+
+    public function testChildrenSeriesByParentWithLimit()
+    {
+        $this->loadFixtures(['MongrelsFixture']);
+        $repo = $this->getEntityManager()->getRepository('ProgrammesPagesService:CoreEntity');
+
+        $dbid = $this->getDbIdFromPid('b010t19z', 'Brand');
+
+        $entities = $repo->findChildrenSeriesByParent($dbid, 1, 0);
+
+        $expectedPids = ['b00swyx1'];
+        $this->assertEquals($expectedPids, array_column($entities, 'pid'));
+
+        $this->assertCount(2, $this->getDbQueries());
+    }
+
+    public function testChildrenSeriesByParentWithPage()
+    {
+        $this->loadFixtures(['MongrelsFixture']);
+        $repo = $this->getEntityManager()->getRepository('ProgrammesPagesService:CoreEntity');
+
+        $dbid = $this->getDbIdFromPid('b010t19z', 'Brand');
+
+        $entities = $repo->findChildrenSeriesByParent($dbid, 50, 1);
+
+        $expectedPids = ['b010t150'];
+        $this->assertEquals($expectedPids, array_column($entities, 'pid'));
+
         $this->assertCount(2, $this->getDbQueries());
     }
 }
