532c958558bed406cb55cf016963f3eff88bd8d2
Francisco Albusac <albusf01@mc-h818424.national.core.bbc.co.uk>
2016-12-16 15:48:47 +0000

    Tests

---
+++ tests/Service/ProgrammesService/FindAllByCategoryTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\BBC\ProgrammesPagesService\Service\ProgrammesService;
+
+use BBC\ProgrammesPagesService\Service\ProgrammesService;
+
+class FindAllByCategoryTest extends AbstractProgrammesServiceTest
+{
+    public function testFindAllByCategory()
+    {
+        $medium = 'tv';
+        $dbId = 1;
+        $dbData = [['pid' => 'b00swyx1'], ['pid' => 'b010t150']];
+
+        //$category = new Genre([1,2,3], 3, 'mytitle', 'myurlkey');
+        $category = $this->mockEntity('Genre', $dbId);
+
+        $this->mockRepository->expects($this->once())
+                             ->method('findProgrammesByCategory')
+                             ->with(
+                                 $category->getDbAncestryIds(),
+                                 false,
+                                 $medium,
+                                 ProgrammesService::DEFAULT_LIMIT
+                             )
+                             ->willReturn($dbData);
+
+        $result = $this->service()->findAllByCategory($category, $medium);
+        $this->assertEquals($this->programmesFromDbData($dbData), $result);
+    }
+}
---
+++ tests/Service/ProgrammesService/FindAvailableByCategoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\BBC\ProgrammesPagesService\Service\ProgrammesService;
+
+use BBC\ProgrammesPagesService\Service\ProgrammesService;
+
+class FindAvailableByCategoryTest extends AbstractProgrammesServiceTest
+{
+    public function testFindAvailableByCategory()
+    {
+        $medium = 'tv';
+        $dbId = 1;
+        $dbData = [['pid' => 'b00swyx1'], ['pid' => 'b010t150']];
+        $category = $this->mockEntity('Genre', $dbId);
+
+        $this->mockRepository->expects($this->once())
+                             ->method('findProgrammesByCategory')
+                             ->with(
+                                 $category->getDbAncestryIds(),
+                                 true,
+                                 $medium,
+                                 ProgrammesService::DEFAULT_LIMIT
+                             )
+                             ->willReturn($dbData);
+
+        $result = $this->service()->findAvailableByCategory($category, $medium);
+        $this->assertEquals($this->programmesFromDbData($dbData), $result);
+    }
+}
