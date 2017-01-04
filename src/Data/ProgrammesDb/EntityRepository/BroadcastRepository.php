f57f2576e206c88030623ec4e7aa43b4d27fd00a
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-01-04 15:27:37 +0000

    Fix

--- src/Data/ProgrammesDb/EntityRepository/BroadcastRepository.php
+++ src/Data/ProgrammesDb/EntityRepository/BroadcastRepository.php
@@ -68,7 +68,10 @@ class BroadcastRepository extends EntityRepository
     ) {
         $qb = $this->createCollapsedBroadcastsOfCategoryQueryBuilder($categoryAncestry, $type);
 
-        $qb->andWhere('broadcast.endAt > :from')
+        $qb->addSelect(['masterBrand', 'network'])
+            ->leftJoin('programmeItem.masterBrand', 'masterBrand')
+            ->leftJoin('masterBrand.network', 'network')
+            ->andWhere('broadcast.endAt > :from')
             ->andWhere('broadcast.endAt <= :to')
             ->addOrderBy('broadcast.startAt')
             ->addOrderBy('networkOfService.urlKey')
