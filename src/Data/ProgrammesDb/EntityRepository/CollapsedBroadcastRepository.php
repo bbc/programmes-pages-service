31d413aadc207119e4e18e854750513a47230ff0
Bjorn Twachtmann <bjorn.twachtmann@bbc.co.uk>
2017-02-02 11:48:08 +0000

    rough sketch of what this might look like

--- src/Data/ProgrammesDb/EntityRepository/CollapsedBroadcastRepository.php
+++ src/Data/ProgrammesDb/EntityRepository/CollapsedBroadcastRepository.php
@@ -2,14 +2,21 @@
 
 namespace BBC\ProgrammesPagesService\Data\ProgrammesDb\EntityRepository;
 
-use Doctrine\ORM\EntityRepository;
-
 class CollapsedBroadcastRepository extends EntityRepository
 {
-    public const NO_SERVICE = 'NULL';
-
-    public function createQueryBuilder($alias, $indexBy = null)
+    public function createQueryBuilder($alias, $joinViaVersion = true, $indexBy = null)
     {
+        // Any time Broadcasts are fetched here they must be inner joined to
+        // their programme entity - either directly - or via the version, this
+        // allows the embargoed filter to trigger and exclude unwanted items.
+        // This ensures that Broadcasts that belong to a version that belongs
+        // to an embargoed programme are never returned
+        if ($joinViaVersion) {
+            return parent::createQueryBuilder($alias)
+                ->join($alias . '.version', 'version')
+                ->join('version.programmeItem', 'programmeItem');
+        }
+
         return parent::createQueryBuilder($alias)
             ->join($alias . '.programmeItem', 'programmeItem');
     }
