e8e1d13e56874a15ad3556012ed9c4f93222096e
Ben Scott <ben.scott@bbc.co.uk>
2017-02-17 11:17:18 +0000

    Ensure PHPUnit can be ran from an working directory

--- tests/phpunit-bootstrap.php
+++ tests/phpunit-bootstrap.php
@@ -6,5 +6,5 @@ error_reporting(E_ALL | E_STRICT);
 date_default_timezone_set('UTC');
 
 // Make sure we've got a DB with a schema in it
-system('vendor/bin/doctrine orm:schema-tool:drop --force > /dev/null');
-system('vendor/bin/doctrine orm:schema-tool:create > /dev/null');
+system(__DIR__ . '/../vendor/bin/doctrine orm:schema-tool:drop --force > /dev/null');
+system(__DIR__ . '/../vendor/bin/doctrine orm:schema-tool:create > /dev/null');
