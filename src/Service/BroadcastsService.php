77c395e0f5e0c3a9d58642b9db4b7a2bb0c0b9ee
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-05-08 16:32:24 +0100

    Function to fetch by using the service all the broadcasts by date

--- src/Service/BroadcastsService.php
+++ src/Service/BroadcastsService.php
@@ -3,15 +3,17 @@
 namespace BBC\ProgrammesPagesService\Service;
 
 use BBC\ProgrammesPagesService\Data\ProgrammesDb\EntityRepository\BroadcastRepository;
-use BBC\ProgrammesPagesService\Domain\Entity\Category;
+use BBC\ProgrammesPagesService\Domain\Entity\Service;
 use BBC\ProgrammesPagesService\Domain\Entity\Version;
-use BBC\ProgrammesPagesService\Domain\Entity\Programme;
 use BBC\ProgrammesPagesService\Mapper\ProgrammesDbToDomain\BroadcastMapper;
 use DateTimeImmutable;
 use Psr\Cache\CacheItemPoolInterface;
 
 class BroadcastsService extends AbstractService
 {
+    /** @var  BroadcastRepository */
+    protected $repository;
+
     public function __construct(
         BroadcastRepository $repository,
         BroadcastMapper $mapper,
@@ -30,6 +32,22 @@ class BroadcastsService extends AbstractService
             'Broadcast',
             $limit,
             $this->getOffset($limit, $page)
+        );
+
+        return $this->mapManyEntities($dbEntities);
+    }
+
+    public function findByServiceAndDateRange(
+        $serviceId,
+        DateTimeImmutable $startDate,
+        DateTimeImmutable $endDate,
+        ?int $limit = self::DEFAULT_LIMIT
+    ) {
+        $dbEntities = $this->repository->findAllByServiceAndDateRange(
+            $serviceId,
+            $startDate,
+            $endDate,
+            $limit
         );
 
         return $this->mapManyEntities($dbEntities);
