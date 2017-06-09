bebce3ac7877ed7fe9c81352585c7955263a1980
Ben Scott <ben.scott@bbc.co.uk>
2017-06-09 10:44:05 +0100

    Add strict types to BroadcastGap and test

--- src/Domain/Entity/BroadcastGap.php
+++ src/Domain/Entity/BroadcastGap.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types = 1);
 namespace BBC\ProgrammesPagesService\Domain\Entity;
 
 use BBC\ProgrammesPagesService\Domain\ApplicationTime;
--- tests/Domain/Entity/BroadcastGapTest.php
+++ tests/Domain/Entity/BroadcastGapTest.php
@@ -1,5 +1,5 @@
 <?php
-
+declare(strict_types = 1);
 namespace Tests\BBC\ProgrammesPagesService\Domain\Entity;
 
 use BBC\ProgrammesPagesService\Domain\Entity\BroadcastGap;
