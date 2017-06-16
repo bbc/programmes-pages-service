a010250bab0fa77e938a5c083eac1a145045c777
Felipe Araujo <felipe.peixotodearaujo@bbc.co.uk>
2017-06-16 17:46:59 +0100

    Fixing tests

--- tests/Data/ProgrammesDb/Entity/RefOptionsTest.php
+++ tests/Data/ProgrammesDb/Entity/RefOptionsTest.php
@@ -3,6 +3,7 @@
 namespace Tests\BBC\ProgrammesPagesService\Data\ProgrammesDb\Entity;
 
 use BBC\ProgrammesPagesService\Data\ProgrammesDb\Entity\RefOptions;
+use DateTime;
 use PHPUnit\Framework\TestCase;
 
 class RefOptionsTest extends TestCase
@@ -13,7 +14,9 @@ class RefOptionsTest extends TestCase
             'guid',
             'projectid',
             'entityId',
-            'admin'
+            'admin',
+            new DateTime('U'),
+            new DateTime('U')
         );
 
         $this->assertSame([], $options->getOptions());
@@ -28,7 +31,9 @@ class RefOptionsTest extends TestCase
             'guid',
             'entityId',
             'projectid',
-            'wrong type'
+            'wrong type',
+            new DateTime('U'),
+            new DateTime('U')
         );
     }
 }
