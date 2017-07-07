144004a6aa85aed0de8a0be69e1f577a2d9d7526
Felipe Araujo <felipe.peixotodearaujo@bbc.co.uk>
2017-07-07 11:01:04 +0100

    Updating iSite default values

--- src/Mapper/ProgrammesDbToDomain/OptionsMapper.php
+++ src/Mapper/ProgrammesDbToDomain/OptionsMapper.php
@@ -7,6 +7,7 @@ use BBC\ProgrammesPagesService\Domain\Entity\Options;
 class OptionsMapper extends AbstractMapper
 {
     private const OPTIONS_DEFAULT_SCHEMA = [
+        // admin options
         'branding_id' => [ 'default' => 'br-00002', 'cascades' => true ],
         'language' => [ 'default' => 'en', 'cascades' => true ],
         'pulse_survey' => [ 'default' => null, 'cascades' => true ],
@@ -16,11 +17,14 @@ class OptionsMapper extends AbstractMapper
         'show_clip_cards' => [ 'default' => true, 'cascades' => true ],
         'show_gallery_cards' => [ 'default' => true, 'cascades' => true ],
         'double_width_first_promo' => [ 'default' => false, 'cascades' => true ],
-        'pid_override' => [ 'default' => null, 'cascades' => true ],
+        'pid_override_url' => [ 'default' => null, 'cascades' => true ],
+        'pid_override_code' => [ 'default' => null, 'cascades' => true ],
         'show_tracklist_inadvance' => [ 'default' => false, 'cascades' => true ],
         'show_tracklist_timings' => [ 'default' => false, 'cascades' => true ],
         'promoted_programmes' => [ 'default' => [], 'cascades' => false ],
         'show_enhanced_navigation' => [ 'default' => false, 'cascades' => true ],
+
+        // local options
         'comments_clips_id' => [ 'default' => null, 'cascades' => true ],
         'comments_clips_enabled' => [ 'default' => false, 'cascades' => true ],
         'comments_episodes_id' => [ 'default' => null, 'cascades' => true ],
@@ -29,9 +33,13 @@ class OptionsMapper extends AbstractMapper
         'recipes_enabled' => [ 'default' => false, 'cascades' => true ],
         'live_stream_id' => [ 'default' => null, 'cascades' => false ],
         'live_stream_heading' => [ 'default' => null, 'cascades' => false ],
-        'ivote' => [ 'default' => null, 'cascades' => false ],
-        'coming_soon' => [ 'default' => null, 'cascades' => false ],
+        'ivote_block' => [ 'default' => null, 'cascades' => false ],
+        'comingsoon_block' => [ 'default' => null, 'cascades' => false ],
+        'comingsoon_textonly' => [ 'default' => null, 'cascades' => false ],
         'navigation_links' => [ 'default' => [], 'cascades' => false ],
+        'bbc_site' => [ 'default' => null, 'cascades' => false ],
+        'livepromo_block' => [ 'default' => null, 'cascades' => false ],
+        'prioritytext_block' => [ 'default' => null, 'cascades' => false ],
     ];
 
     private static $defaults = [];
