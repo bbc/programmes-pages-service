9817cd27fcfb957b7ceb639c9cfbbcb7671121a9
Michael Gwynne <michael.gwynne@bbc.co.uk>
2017-08-07 10:01:28 +0100

    Forgot group Tlec and Tleo

--- src/Domain/Entity/Group.php
+++ src/Domain/Entity/Group.php
@@ -4,5 +4,13 @@ namespace BBC\ProgrammesPagesService\Domain\Entity;
 
 abstract class Group extends CoreEntity
 {
+    public function isTlec(): bool
+    {
+        return true;
+    }
 
+    public function isTleo(): bool
+    {
+        return true;
+    }
 }
