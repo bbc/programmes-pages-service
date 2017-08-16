02e96359223a4ace6abce357b783492d7113317b
Francisco Albert <francis.jaa@gmail.com>
2017-08-16 16:02:26 +0100

    changed strings literal for class constants in factories

--- src/Service/ServiceFactory.php
+++ src/Service/ServiceFactory.php
@@ -30,47 +30,47 @@ class ServiceFactory
 
     public function getAtozTitlesService(): AtozTitlesService
     {
-        if (!isset($this->instances['AtozTitlesService'])) {
-            $this->instances['AtozTitlesService'] = new AtozTitlesService(
+        if (!isset($this->instances[AtozTitlesService::class])) {
+            $this->instances[AtozTitlesService::class] = new AtozTitlesService(
                 $this->entityManager->getRepository('ProgrammesPagesService:AtozTitle'),
                 $this->mapperFactory->getAtozTitleMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['AtozTitlesService'];
+        return $this->instances[AtozTitlesService::class];
     }
 
     public function getBroadcastsService(): BroadcastsService
     {
-        if (!isset($this->instances['BroadcastsService'])) {
-            $this->instances['BroadcastsService'] = new BroadcastsService(
+        if (!isset($this->instances[BroadcastsService::class])) {
+            $this->instances[BroadcastsService::class] = new BroadcastsService(
                 $this->entityManager->getRepository('ProgrammesPagesService:Broadcast'),
                 $this->mapperFactory->getBroadcastMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['BroadcastsService'];
+        return $this->instances[BroadcastsService::class];
     }
 
     public function getCategoriesService(): CategoriesService
     {
-        if (!isset($this->instances['CategoriesService'])) {
-            $this->instances['CategoriesService'] = new CategoriesService(
+        if (!isset($this->instances[CategoriesService::class])) {
+            $this->instances[CategoriesService::class] = new CategoriesService(
                 $this->entityManager->getRepository('ProgrammesPagesService:Category'),
                 $this->mapperFactory->getCategoryMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['CategoriesService'];
+        return $this->instances[CategoriesService::class];
     }
 
     public function getCollapsedBroadcastsService(): CollapsedBroadcastsService
     {
-        if (!isset($this->instances['CollapsedBroadcastsService'])) {
-            $this->instances['CollapsedBroadcastsService'] = new CollapsedBroadcastsService(
+        if (!isset($this->instances[CollapsedBroadcastsService::class])) {
+            $this->instances[CollapsedBroadcastsService::class] = new CollapsedBroadcastsService(
                 $this->entityManager->getRepository('ProgrammesPagesService:CollapsedBroadcast'),
                 $this->mapperFactory->getCollapsedBroadcastMapper(),
                 $this->cache,
@@ -78,162 +78,162 @@ class ServiceFactory
             );
         }
 
-        return $this->instances['CollapsedBroadcastsService'];
+        return $this->instances[CollapsedBroadcastsService::class];
     }
 
     public function getContributionsService(): ContributionsService
     {
-        if (!isset($this->instances['ContributionsService'])) {
-            $this->instances['ContributionsService'] = new ContributionsService(
+        if (!isset($this->instances[ContributionsService::class])) {
+            $this->instances[ContributionsService::class] = new ContributionsService(
                 $this->entityManager->getRepository('ProgrammesPagesService:Contribution'),
                 $this->mapperFactory->getContributionMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['ContributionsService'];
+        return $this->instances[ContributionsService::class];
     }
 
     public function getContributorsService(): ContributorsService
     {
-        if (!isset($this->instances['ContributorsService'])) {
-            $this->instances['ContributorsService'] = new ContributorsService(
+        if (!isset($this->instances[ContributorsService::class])) {
+            $this->instances[ContributorsService::class] = new ContributorsService(
                 $this->entityManager->getRepository('ProgrammesPagesService:Contributor'),
                 $this->mapperFactory->getContributorMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['ContributorsService'];
+        return $this->instances[ContributorsService::class];
     }
 
     public function getCoreEntitiesService(): CoreEntitiesService
     {
-        if (!isset($this->instances['CoreEntitiesService'])) {
-            $this->instances['CoreEntitiesService'] = new CoreEntitiesService(
+        if (!isset($this->instances[CoreEntitiesService::class])) {
+            $this->instances[CoreEntitiesService::class] = new CoreEntitiesService(
                 $this->entityManager->getRepository('ProgrammesPagesService:CoreEntity'),
                 $this->mapperFactory->getCoreEntityMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['CoreEntitiesService'];
+        return $this->instances[CoreEntitiesService::class];
     }
 
     public function getGroupsService(): GroupsService
     {
-        if (!isset($this->instances['GroupsService'])) {
-            $this->instances['GroupsService'] = new GroupsService(
+        if (!isset($this->instances[GroupsService::class])) {
+            $this->instances[GroupsService::class] = new GroupsService(
                 $this->entityManager->getRepository('ProgrammesPagesService:CoreEntity'),
                 $this->mapperFactory->getCoreEntityMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['GroupsService'];
+        return $this->instances[GroupsService::class];
     }
 
     public function getNetworksService(): NetworksService
     {
-        if (!isset($this->instances['NetworksService'])) {
-            $this->instances['NetworksService'] = new NetworksService(
+        if (!isset($this->instances[NetworksService::class])) {
+            $this->instances[NetworksService::class] = new NetworksService(
                 $this->entityManager->getRepository('ProgrammesPagesService:Network'),
                 $this->mapperFactory->getNetworkMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['NetworksService'];
+        return $this->instances[NetworksService::class];
     }
 
     public function getProgrammesAggregationService(): ProgrammesAggregationService
     {
-        if (!isset($this->instances['ProgrammesAggregationService'])) {
-            $this->instances['ProgrammesAggregationService'] = new ProgrammesAggregationService(
+        if (!isset($this->instances[ProgrammesAggregationService::class])) {
+            $this->instances[ProgrammesAggregationService::class] = new ProgrammesAggregationService(
                 $this->entityManager->getRepository('ProgrammesPagesService:CoreEntity'),
                 $this->mapperFactory->getCoreEntityMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['ProgrammesAggregationService'];
+        return $this->instances[ProgrammesAggregationService::class];
     }
 
     public function getProgrammesService(): ProgrammesService
     {
-        if (!isset($this->instances['ProgrammesService'])) {
-            $this->instances['ProgrammesService'] = new ProgrammesService(
+        if (!isset($this->instances[ProgrammesService::class])) {
+            $this->instances[ProgrammesService::class] = new ProgrammesService(
                 $this->entityManager->getRepository('ProgrammesPagesService:CoreEntity'),
                 $this->mapperFactory->getCoreEntityMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['ProgrammesService'];
+        return $this->instances[ProgrammesService::class];
     }
 
     public function getRelatedLinksService(): RelatedLinksService
     {
-        if (!isset($this->instances['RelatedLinksService'])) {
-            $this->instances['RelatedLinksService'] = new RelatedLinksService(
+        if (!isset($this->instances[RelatedLinksService::class])) {
+            $this->instances[RelatedLinksService::class] = new RelatedLinksService(
                 $this->entityManager->getRepository('ProgrammesPagesService:RelatedLink'),
                 $this->mapperFactory->getRelatedLinkMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['RelatedLinksService'];
+        return $this->instances[RelatedLinksService::class];
     }
 
     public function getSegmentEventsService(): SegmentEventsService
     {
-        if (!isset($this->instances['SegmentEventsService'])) {
-            $this->instances['SegmentEventsService'] = new SegmentEventsService(
+        if (!isset($this->instances[SegmentEventsService::class])) {
+            $this->instances[SegmentEventsService::class] = new SegmentEventsService(
                 $this->entityManager->getRepository('ProgrammesPagesService:SegmentEvent'),
                 $this->mapperFactory->getSegmentEventMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['SegmentEventsService'];
+        return $this->instances[SegmentEventsService::class];
     }
 
     public function getSegmentsService(): SegmentsService
     {
-        if (!isset($this->instances['SegmentsService'])) {
-            $this->instances['SegmentsService'] = new SegmentsService(
+        if (!isset($this->instances[SegmentsService::class])) {
+            $this->instances[SegmentsService::class] = new SegmentsService(
                 $this->entityManager->getRepository('ProgrammesPagesService:Segment'),
                 $this->mapperFactory->getSegmentMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['SegmentsService'];
+        return $this->instances[SegmentsService::class];
     }
 
     public function getServicesService(): ServicesService
     {
-        if (!isset($this->instances['ServicesService'])) {
-            $this->instances['ServicesService'] = new ServicesService(
+        if (!isset($this->instances[ServicesService::class])) {
+            $this->instances[ServicesService::class] = new ServicesService(
                 $this->entityManager->getRepository('ProgrammesPagesService:Service'),
                 $this->mapperFactory->getServiceMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['ServicesService'];
+        return $this->instances[ServicesService::class];
     }
 
     public function getVersionsService(): VersionsService
     {
-        if (!isset($this->instances['VersionsService'])) {
-            $this->instances['VersionsService'] = new VersionsService(
+        if (!isset($this->instances[VersionsService::class])) {
+            $this->instances[VersionsService::class] = new VersionsService(
                 $this->entityManager->getRepository('ProgrammesPagesService:Version'),
                 $this->mapperFactory->getVersionMapper(),
                 $this->cache
             );
         }
 
-        return $this->instances['VersionsService'];
+        return $this->instances[VersionsService::class];
     }
 }
