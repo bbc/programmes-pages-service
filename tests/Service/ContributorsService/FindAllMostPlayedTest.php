2425acf923262c9f405fb13e6a6f926ddb17eb75
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-09-05 14:08:47 +0100

    more

--- tests/Service/ContributorsService/FindAllMostPlayedTest.php
+++ tests/Service/ContributorsService/FindAllMostPlayedTest.php
@@ -31,11 +31,11 @@ class FindAllMostPlayedTest extends AbstractContributorsServiceTest
     public function serviceProvider(): array
     {
         return [
-            'CASE: when passing a service' => [
+            'CASE: expectations when passing a service' => [
                 999,
                 [new Service(999, new Sid('sid'), new Pid('b0000001'), 'name')],
             ],
-            'CASE: when no passing a service' => [
+            'CASE: expectations when no passing a service' => [
                 null,
                 [],
             ],
