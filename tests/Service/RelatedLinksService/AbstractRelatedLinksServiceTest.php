e8d75491b529df8990496ccb46a8ba86ea75b7cd
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-09-06 15:00:48 +0100

    related links

--- tests/Service/RelatedLinksService/AbstractRelatedLinksServiceTest.php
+++ tests/Service/RelatedLinksService/AbstractRelatedLinksServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\BBC\ProgrammesPagesService\Service\RelatedLinksService;
 
+use BBC\ProgrammesPagesService\Domain\Entity\RelatedLink;
 use BBC\ProgrammesPagesService\Service\RelatedLinksService;
 use Tests\BBC\ProgrammesPagesService\AbstractServiceTest;
 
@@ -11,20 +12,9 @@ abstract class AbstractRelatedLinksServiceTest extends AbstractServiceTest
     {
         $this->setUpCache();
         $this->setUpRepo('RelatedLinkRepository');
-        $this->setUpMapper('RelatedLinkMapper', 'relatedLinkFromDbData');
-    }
-
-    protected function relatedLinksFromDbData(array $entities)
-    {
-        return array_map([$this, 'relatedLinkFromDbData'], $entities);
-    }
-
-    protected function relatedLinkFromDbData(array $entity)
-    {
-        $mockRelatedLink = $this->createMock(self::ENTITY_NS . 'RelatedLink');
-
-        $mockRelatedLink->method('getTitle')->willReturn($entity['title']);
-        return $mockRelatedLink;
+        $this->setUpMapper('RelatedLinkMapper', function ($dbRelatedLink) {
+            return $this->createConfiguredMock(RelatedLink::class, ['getTitle' => $dbRelatedLink['title']]);
+        });
     }
 
     protected function service()
