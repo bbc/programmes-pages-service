855dfce38f302a1e394a7b2af3517be236074930
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-09-06 15:32:37 +0100

    segments done

--- tests/Service/SegmentsService/FindByPidFullTest.php
+++ tests/Service/SegmentsService/FindByPidFullTest.php
@@ -2,34 +2,39 @@
 
 namespace Tests\BBC\ProgrammesPagesService\Service\SegmentsService;
 
+use BBC\ProgrammesPagesService\Domain\Entity\Segment;
 use BBC\ProgrammesPagesService\Domain\ValueObject\Pid;
 
 class FindByPidFullTest extends AbstractSegmentsServiceTest
 {
-    public function testFindByPidFull()
+    public function testCommunicationWithRepository()
     {
         $pid = new Pid('s1234567');
-        $dbData = ['pid' => 's1234567'];
 
         $this->mockRepository->expects($this->once())
             ->method('findByPidFull')
-            ->with($pid)
-            ->willReturn($dbData);
+            ->with($pid);
 
-        $result = $this->service()->findByPidFull($pid);
-        $this->assertEquals($this->segmentFromDbData($dbData), $result);
+        $this->service()->findByPidFull($pid);
     }
+
+    public function testResultsFoundaaa()
+    {
+        $this->mockRepository->method('findByPidFull')->willReturn(['pid' => 's1234567']);
+
+        $segment = $this->service()->findByPidFull($this->createMock(Pid::class));
+
+        $this->assertInstanceOf(Segment::class, $segment);
+        $this->assertEquals('s1234567', $segment->getPid());
+    }
+
 
     public function testFindByPidFullEmptyData()
     {
-        $pid = new Pid('s1234567');
+        $this->mockRepository->method('findByPidFull')->willReturn(null);
 
-        $this->mockRepository->expects($this->once())
-            ->method('findByPidFull')
-            ->with($pid)
-            ->willReturn(null);
+        $segment = $this->service()->findByPidFull($this->createMock(Pid::class));
 
-        $result = $this->service()->findByPidFull($pid);
-        $this->assertEquals(null, $result);
+        $this->assertNull($segment);
     }
 }
