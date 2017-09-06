ececa327f73d948eda11304af5f009da882665d7
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-09-06 15:04:23 +0100

    more

--- tests/Service/RelatedLinksService/FindByRelatedToProgrammeTest.php
+++ tests/Service/RelatedLinksService/FindByRelatedToProgrammeTest.php
@@ -57,7 +57,7 @@ class FindByRelatedToProgrammeTest extends AbstractRelatedLinksServiceTest
             ],
             'CASE: ' => [
                 [],
-                []
+                [],
             ],
         ];
     }
