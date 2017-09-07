1eaadf7bfaa80e65af28d7eb28ce76494d5579f1
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-09-07 10:13:25 +0100

    more

--- tests/Service/ProgrammesService/FindAllTleosByCategoryTest.php
+++ tests/Service/ProgrammesService/FindAllTleosByCategoryTest.php
@@ -2,27 +2,54 @@
 
 namespace Tests\BBC\ProgrammesPagesService\Service\ProgrammesService;
 
+use BBC\ProgrammesPagesService\Domain\Entity\Genre;
+use BBC\ProgrammesPagesService\Domain\Entity\Programme;
 use BBC\ProgrammesPagesService\Service\ProgrammesService;
 
 class FindAllTleosByCategoryTest extends AbstractProgrammesServiceTest
 {
-    public function testFindAllTleosByCategory()
+    public function testFind()
     {
-        $dbId = 1;
-        $dbData = [['pid' => 'b00swyx1'], ['pid' => 'b010t150']];
-
-        $category = $this->mockEntity('Genre', $dbId);
+        $category = $this->createConfiguredMock(Genre::class, ['getDbId' => 1]);
 
         $this->mockRepository->expects($this->once())
             ->method('findTleosByCategory')
             ->with(
-                $category->getDbAncestryIds(),
-                false,
-                ProgrammesService::DEFAULT_LIMIT
-            )
-            ->willReturn($dbData);
+                 $category->getDbAncestryIds(),
+                 false,
+                 ProgrammesService::DEFAULT_LIMIT
+            );
 
-        $result = $this->service()->findAllTleosByCategory($category);
-        $this->assertEquals($this->programmesFromDbData($dbData), $result);
+        $this->service()->findAllTleosByCategory($category);
+    }
+
+    /**
+     * @dataProvider dbTleosProvider
+     */
+    public function testFindAllTleosByCategory(array $expectedPids, array $dbTleosProvided)
+    {
+        $this->mockRepository->method('findTleosByCategory')->willReturn($dbTleosProvided);
+
+        $tleos = $this->service()->findAllTleosByCategory($this->createMock(Genre::class));
+
+        $this->assertContainsOnlyInstancesOf(Programme::class, $tleos);
+        $this->assertCount(count($expectedPids), $tleos);
+        foreach ($expectedPids as $i => $expectedPid) {
+            $this->assertEquals($expectedPid, $tleos[$i]->getPid());
+        }
+    }
+
+    public function dbTleosProvider(): array
+    {
+        return [
+            'CASE: ' => [
+                ['b010t19z', 'b00swyx1'],
+                [['pid' => 'b010t19z'], ['pid' => 'b00swyx1']],
+            ],
+            'CASE: ' => [
+                [],
+                [],
+            ],
+        ];
     }
 }
