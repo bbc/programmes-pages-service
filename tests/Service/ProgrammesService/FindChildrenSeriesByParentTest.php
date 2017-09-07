1821585adc83d7e674ab83f1e046993d704d11db
Francisco Albusac <francisco.albusac@bbc.co.uk>
2017-09-07 11:26:10 +0100

    change

--- tests/Service/ProgrammesService/FindChildrenSeriesByParentTest.php
+++ tests/Service/ProgrammesService/FindChildrenSeriesByParentTest.php
@@ -2,23 +2,55 @@
 
 namespace Tests\BBC\ProgrammesPagesService\Service\ProgrammesService;
 
+use BBC\ProgrammesPagesService\Domain\Entity\Programme;
 use BBC\ProgrammesPagesService\Domain\Entity\ProgrammeContainer;
 
 class FindChildrenSeriesByParentTest extends AbstractProgrammesServiceTest
 {
-    public function testFindAllSeriesByParent()
+    public function testRepositoryReceiveAllParamsToFetchData()
     {
-        $container = $this->createMock(ProgrammeContainer::class);
-        $container->method('getDbId')->willReturn(0);
-
-        $dbData = [['pid' => 'b010t19z'], ['pid' => 'b00swyx1']];
+        $programmeContainerParent = $this->createConfiguredMock(ProgrammeContainer::class, ['getDbId' => 0]);
 
         $this->mockRepository->expects($this->once())
             ->method('findChildrenSeriesByParent')
-            ->with(0)
-            ->willReturn($dbData);
+            ->with($programmeContainerParent->getDbId());
 
-        $result = $this->service()->findChildrenSeriesByParent($container);
-        $this->assertEquals($this->programmesFromDbData($dbData), $result);
+        $this->service()->findChildrenSeriesByParent($programmeContainerParent);
+    }
+
+    /**
+     * @dataProvider dbSeriesProvider
+     */
+    public function testArrayIsReturnedWithSeriesOnIt(array $expectedPids, array $dbSeriesProvided)
+    {
+        $programmeContainerParent = $this->createConfiguredMock(ProgrammeContainer::class, ['getDbId' => 0]);
+
+        $this->mockRepository
+            ->method('findChildrenSeriesByParent')
+            ->willReturn($dbSeriesProvided);
+
+        $series = $this->service()->findChildrenSeriesByParent($programmeContainerParent);
+
+        // we should test that is a serie, but that funcionality is really tested by the mapper. In here we test
+        // the most generic case
+        $this->assertContainsOnlyInstancesOf(Programme::class, $series);
+        $this->assertCount(count($dbSeriesProvided), $series);
+        foreach ($expectedPids as $i => $expectedPid) {
+            $this->assertEquals($expectedPid, $series[$i]->getPid());
+        }
+    }
+
+    public function dbSeriesProvider(): array
+    {
+        return [
+            'CASE: series found in db' => [
+                ['b010t19z', 'b00swyx1'],
+                [['pid' => 'b010t19z'], ['pid' => 'b00swyx1']],
+            ],
+            'CASE: no series found' => [
+                [],
+                []
+            ],
+        ];
     }
 }
