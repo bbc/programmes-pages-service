ab7cf525934a1712bb724668286aa87449451fab
Michael Gwynne <michael.gwynne@bbc.co.uk>
2018-03-23 09:01:56 +0000

    Removing programmeItem info from query

--- src/Data/ProgrammesDb/EntityRepository/CollapsedBroadcastRepository.php
+++ src/Data/ProgrammesDb/EntityRepository/CollapsedBroadcastRepository.php
@@ -174,15 +174,11 @@ QUERY;
         int $offset
     ): array {
         $qb = $this->createQueryBuilder('collapsedBroadcast', false)
-            ->addSelect(['programmeItem', 'image', 'masterBrand', 'mbNetwork'])
-            ->leftJoin('programmeItem.image', 'image')
-            ->leftJoin('programmeItem.masterBrand', 'masterBrand')
-            ->leftJoin('masterBrand.network', 'mbNetwork')
             ->andWhere('collapsedBroadcast.isWebcastOnly = :isWebcastOnly')
             ->andWhere('programmeItem.id = :programmeItemId')
             ->setParameter('programmeItemId', $programmeId)
             ->setParameter('isWebcastOnly', $isWebcastOnly)
-            ->addOrderBy('collapsedBroadcast.endAt', 'ASC')
+            ->addOrderBy('collapsedBroadcast.startAt', 'ASC')
             ->setFirstResult($offset)
             ->setMaxResults($limit);
 
