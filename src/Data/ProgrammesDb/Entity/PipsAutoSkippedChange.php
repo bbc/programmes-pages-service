d5320e14d22fb1a0b0133e66364d0b9e39edc218
Bjorn Twachtmann <bjorn.twachtmann@bbc.co.uk>
2018-04-16 17:09:53 +0100

    Add new entity to track changes skipped due to parents in another partner_pid

---
+++ src/Data/ProgrammesDb/Entity/PipsAutoSkippedChange.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace BBC\ProgrammesPagesService\Data\ProgrammesDb\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * This table lists all changes relating to our desired partner_pids that have been skipped due to having a parent in
+ * another partner_pid we do not ingest.
+ * It's here so we have a permanent record of when that happens and can re-ingest if necessary.
+ *
+ * We do this because there's really no sane way to deal with that case other than "ingest everything in all
+ * partner_pids", which would require a massive rewrite of our queries and reingest of all data.
+ *
+ * @ORM\Table(indexes={
+ *     @ORM\Index(name="processed_time_idx", columns={"processed_time"}),
+ * })
+ * @ORM\Entity(repositoryClass="BBC\ProgrammesPagesService\Data\ProgrammesDb\EntityRepository\PipsAutoSkippedChangeRepository")
+ */
+class PipsAutoSkippedChange extends PipsChangeBase
+{
+    /**
+     * The ID of the parent entity that caused this (otherwise valid) change event to be auto-skipped.
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $triggeringEntityId;
+
+    /**
+     * @return string
+     */
+    public function getTriggeringEntityId(): string
+    {
+        return $this->triggeringEntityId;
+    }
+
+    /**
+     * @param string $triggeringEntityId
+     */
+    public function setTriggeringEntityId(string $triggeringEntityId)
+    {
+        $this->triggeringEntityId = $triggeringEntityId;
+    }
+}
