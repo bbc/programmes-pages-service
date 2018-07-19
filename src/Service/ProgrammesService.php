77648b01d856145a6c210773f1446b1a651d1627
Francisco Albusac <francisco.albusac@bbc.co.uk>
2018-07-19 14:40:50 +0100

    removed usless USE

--- src/Service/ProgrammesService.php
+++ src/Service/ProgrammesService.php
@@ -14,7 +14,6 @@ use BBC\ProgrammesPagesService\Domain\Entity\ProgrammeItem;
 use BBC\ProgrammesPagesService\Domain\Entity\Series;
 use BBC\ProgrammesPagesService\Domain\ValueObject\Pid;
 use BBC\ProgrammesPagesService\Mapper\ProgrammesDbToDomain\CoreEntityMapper;
-use BBC\ProgrammesPagesService\Mapper\ProgrammesDbToDomain\VersionMapper;
 use InvalidArgumentException;
 
 class ProgrammesService extends AbstractService
