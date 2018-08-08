575c0fcb13cfdef8acf3b191e86a2ec13d74d189
Sam Fry <sam.fry@bbc.co.uk>
2018-08-08 11:04:05 +0100

    Fixing tests

--- tests/Mapper/ProgrammesDbToDomain/CachingTest.php
+++ tests/Mapper/ProgrammesDbToDomain/CachingTest.php
@@ -33,7 +33,7 @@ class CachingTest extends BaseCoreEntityMapperTestCase
             1,
             new Options(
                 [
-                    'projectId' => null,
+                    'project_space' => null,
                     'branding_id' => 'br-00002',
                     'language' => 'en',
                     'pulse_survey' => null,
