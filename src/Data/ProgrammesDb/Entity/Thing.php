0ab87da592f2a78d32918096a517f1f6b52d225e
William Price <William.Price@bbc.co.uk>
2019-11-29 11:34:54 +0000

    add thing table

---
+++ src/Data/ProgrammesDb/Entity/Thing.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace BBC\ProgrammesPagesService\Data\ProgrammesDb\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
+
+/**
+ * @ORM\Entity(repositoryClass="BBC\ProgrammesPagesService\Data\ProgrammesDb\EntityRepository\ThingRepository")
+ */
+class Thing
+{
+    use TimestampableEntity;
+
+    /**
+     * @var string
+     *
+     * @ORM\Id()
+     * @ORM\Column(type="string", length=36, nullable=false)
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="text", nullable=false)
+     */
+    private $preferredLabel;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="text", nullable=false)
+     */
+    private $disambiguationHint;
+
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    public function setId(string $id)
+    {
+        $this->id = $id;
+    }
+
+    public function getPreferredLabel(): string
+    {
+        return $this->preferredLabel;
+    }
+
+    public function setPreferredLabel(string $preferredLabel)
+    {
+        $this->preferredLabel = $preferredLabel;
+    }
+
+    public function getDisambiguationHint(): string
+    {
+        return $this->disambiguationHint;
+    }
+
+    public function setDisambiguationHint(string $disambiguationHint)
+    {
+        $this->disambiguationHint = $disambiguationHint;
+    }
+}
---
+++ src/Data/ProgrammesDb/EntityRepository/ThingRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace BBC\ProgrammesPagesService\Data\ProgrammesDb\EntityRepository;
+
+use Doctrine\ORM\EntityRepository;
+
+class ThingRepository extends EntityRepository
+{
+    
+}
