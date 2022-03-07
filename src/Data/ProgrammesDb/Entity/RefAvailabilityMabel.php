a58383b046936841815a584e72fcd9ae2ff01507
Marcus Hobson <marcus.hobson@bbc.co.uk>
2022-03-07 10:51:37 +0000

    Modified ref_availability_mabel for uniqueness across version_id and media_set_id

--- src/Data/ProgrammesDb/Entity/RefAvailabilityMabel.php
+++ src/Data/ProgrammesDb/Entity/RefAvailabilityMabel.php
@@ -5,11 +5,16 @@ namespace BBC\ProgrammesPagesService\Data\ProgrammesDb\Entity;
 use BBC\ProgrammesPagesService\Domain\Enumeration\AvailabilityStatusEnum;
 use DateTime;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Gedmo\Timestampable\Traits\TimestampableEntity;
 use InvalidArgumentException;
 
 /**
  * @ORM\Entity()
+ * @ORM\Table(name="ref_availability_mabel", uniqueConstraints={
+ *     @ORM\UniqueConstraint(name="UC_version_media_set", columns={"version_id", "media_set_id"})
+ * })
+ * @UniqueEntity(fields={"version","mediaSet"})
  */
 class RefAvailabilityMabel
 {
