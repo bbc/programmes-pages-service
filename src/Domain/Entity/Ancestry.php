c71ef83db658512f0ed06fb0f7215fc7b876c1b4
darceyh03 <darcey.haddow@bbc.co.uk>
2025-02-03 15:22:03 +0000

    indenting somain/entity/ancestry.php

--- src/Domain/Entity/Ancestry.php
+++ src/Domain/Entity/Ancestry.php
@@ -4,26 +4,27 @@ namespace BBC\ProgrammesPagesService\Domain\Entity;
 
 class Ancestry
 {
-  /** @var int */
-  private $coreEntityId;
+    /** @var int */
+    private $coreEntityId;
 
-  /** @var int */
-  private $ancestorId;
+    /** @var int */
+    private $ancestorId;
 
-  public function __construct(
-    int $coreEntityId,
-    int $ancestorId
-  ) {
-    $this->coreEntityId = $coreEntityId;
-    $this->ancestorId = $ancestorId;
-  }
+    public function __construct(
+      int $coreEntityId,
+      int $ancestorId
+    ) {
+      $this->coreEntityId = $coreEntityId;
+      $this->ancestorId = $ancestorId;
+    }
 
-  public function getCoreEntityId(): int
-  {
-      return $this->coreEntityId;
-  }
+    public function getCoreEntityId(): int
+    {
+        return $this->coreEntityId;
+    }
 
-  public function getAncestorId(): int
-  {
-      return $this->ancestorId;
-  } }
+    public function getAncestorId(): int
+    {
+        return $this->ancestorId;
+    } 
+}
